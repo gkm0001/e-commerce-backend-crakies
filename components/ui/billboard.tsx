b093804c687e09917d984a71e5da9df6684c5f8a
Govind Krishna Mishra <professionalgkm@gmail.com>
2024-01-22 04:53:26 +0530

    update

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -4,13 +4,19 @@ interface BillboardProps {
   data: Billboard;
 }
 
-const Billboard: React.FC<BillboardProps> = ({
-  data
-}) => {
-  console
-  return ( 
+const Billboard: React.FC<BillboardProps> = ({ data }) => {
+  // Check if data is null or undefined
+  if (!data) {
+    // Handle the case where data is null or undefined
+    return <div>No data available</div>;
+  }
+
+  return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
+      <div
+        style={{ backgroundImage: `url(${data?.imageUrl})` }}
+        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+      >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data.label}
@@ -18,7 +24,7 @@ const Billboard: React.FC<BillboardProps> = ({
         </div>
       </div>
     </div>
-   );
+  );
 };
 
 export default Billboard;
