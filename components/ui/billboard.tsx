418b6e1c3e52d4b45c6730ade00e4f7c420c9f7a
Govind Krishna Mishra <professionalgkm@gmail.com>
2024-01-22 16:45:15 +0530

    change font

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -8,9 +8,10 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
   // Check if data is null or undefined
   if (!data) {
     // Handle the case where data is null or undefined
-    return <div className="flex items-center justify-center h-screen">
-    <h1 className="text-4xl font-bold">Welcome to Crackies</h1>
-  </div>
+  //   return <div className="flex items-center justify-center mt-2">
+  //   <h1 className="text-4xl font-bold">Welcome to Crackies</h1>
+  // </div>
+     return <div></div>
   }
 
   return (
